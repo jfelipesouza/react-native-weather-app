c84dcbe3e8c1a837742cbfbd2fb113b6717e78f3
jfelipesouza <j.felipe.dev.18@gmail.com>
2024-05-28 00:12:33 -0300

    Substituido o Fragments por uma View

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import {StatusBar, View} from 'react-native';
 import {MainNavigation} from './routers';
 import {colors} from './theme/colors';
 import {BannerComponent} from './components/Banner';
 
 export const MyApp: React.FC = () => {
   return (
-    <>
+    <View style={{backgroundColor: colors.bg, flex: 1}}>
       <StatusBar backgroundColor={colors.bg} barStyle={'light-content'} />
       <BannerComponent />
       <MainNavigation />
-    </>
+    </View>
   );
 };
